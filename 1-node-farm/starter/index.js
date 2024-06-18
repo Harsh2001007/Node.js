6d2e1f1947876c12b32964c6e31263e538cd45b7
Harsh <hs5686584@gmail.com>
2024-06-19 00:30:04 +0530

    async code study

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,32 @@
 const fileModifier = require("fs");
 
-const textIn = fileModifier.readFileSync("./txt/input.txt", "utf-8");
+// const textIn = fileModifier.readFileSync("./txt/input.txt", "utf-8");
 
-console.log(textIn);
+// console.log(textIn);
+
+// const textOut = `About Avocados --> ${textIn} \n by harsh sachan 💪`;
+// fileModifier.writeFileSync("./txt/outputFile.txt", textOut);
+
+// console.log("file written successfully");
+
+// Asynchronus - non blocking way
+
+fileModifier.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  console.log(data1);
+  fileModifier.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fileModifier.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fileModifier.writeFile(
+        "./txt/final.txt",
+        `${data1} \n ${data2} \n ${data3}`,
+        "utf-8",
+        (err) => {
+          console.log("mega file is written");
+        }
+      );
+    });
+  });
+});
+
+console.log("file work done");
