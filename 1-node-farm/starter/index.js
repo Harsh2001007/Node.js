3bc2b78de3bef25c402974f37e2cebaa2a689cb6
Harsh <hs5686584@gmail.com>
2024-06-20 01:04:26 +0530

    added server code

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,8 @@
 const fileModifier = require("fs");
+const http = require("http");
+
+////////////////////////////////////////
+//// Files
 
 // const textIn = fileModifier.readFileSync("./txt/input.txt", "utf-8");
 
@@ -11,22 +15,33 @@ const fileModifier = require("fs");
 
 // Asynchronus - non blocking way
 
-fileModifier.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  console.log(data1);
-  fileModifier.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fileModifier.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-      fileModifier.writeFile(
-        "./txt/final.txt",
-        `${data1} \n ${data2} \n ${data3}`,
-        "utf-8",
-        (err) => {
-          console.log("mega file is written");
-        }
-      );
-    });
-  });
+// fileModifier.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   console.log(data1);
+//   fileModifier.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fileModifier.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fileModifier.writeFile(
+//         "./txt/final.txt",
+//         `${data1} \n ${data2} \n ${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("mega file is written");
+//         }
+//       );
+//     });
+//   });
+// });
+
+// console.log("file work done");
+
+////////////////////////////////////////
+//////// server
+
+const server = http.createServer((req, res) => {
+  res.end("good bye from the server");
 });
 
-console.log("file work done");
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Listening on port 3000");
+});
